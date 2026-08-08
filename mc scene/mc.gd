3fc4999c406fef003841eddf82ce62b7a1ae044e
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-08 19:55:58 +0530

    added the movement animation

--- mc scene/mc.gd
+++ mc scene/mc.gd
@@ -1,16 +1,25 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("left","right","up","down")
-	velocity=direction*SPEED
-
+	var direction := Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED
+	
+	play_animations(direction)
+	
 	move_and_slide()
+
+
+func play_animations(dir: Vector2) -> void:
+	if dir.x > 0:
+		animated_sprite_2d.play("move right")
+	elif dir.x < 0:
+		animated_sprite_2d.play("move left")
+	elif dir.y < 0:
+		animated_sprite_2d.play("move up")
+	elif dir.y > 0:
+		animated_sprite_2d.play("move down")
