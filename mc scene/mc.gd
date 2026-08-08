3615ecb3db163813a9ddc5972d6ad7d7b9452f50
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-08 20:24:41 +0530

    basic movement animation

--- mc scene/mc.gd
+++ mc scene/mc.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 100.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -8,18 +8,24 @@ const SPEED = 300.0
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED
-	
-	play_animations(direction)
-	
+
+	process_animations(direction)
 	move_and_slide()
 
 
-func play_animations(dir: Vector2) -> void:
+func process_animations(direction) -> void:
+	if velocity != Vector2.ZERO:
+		play_animations("move", direction)
+	else:
+		play_animations("idle", direction)
+
+
+func play_animations(prefix: String, dir: Vector2) -> void:
 	if dir.x > 0:
-		animated_sprite_2d.play("move right")
+		animated_sprite_2d.play(prefix + " right")
 	elif dir.x < 0:
-		animated_sprite_2d.play("move left")
+		animated_sprite_2d.play(prefix + " left")
 	elif dir.y < 0:
-		animated_sprite_2d.play("move up")
+		animated_sprite_2d.play(prefix + " up")
 	elif dir.y > 0:
-		animated_sprite_2d.play("move down")
+		animated_sprite_2d.play(prefix + " down")
