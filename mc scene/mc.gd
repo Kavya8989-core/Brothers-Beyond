6caefecc98e73998380fea3729ace4f7845aed72
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-09 02:22:10 +0530

    added attacking animation

--- mc scene/mc.gd
+++ mc scene/mc.gd
@@ -4,11 +4,15 @@ const walk_SPEED = 100.0
 const run_SPEED=200.0
 
 var check_direction: Vector2=Vector2.RIGHT
+var if_attacking :=false
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 
 func _physics_process(_delta: float) -> void:
+	if if_attacking:
+		move_and_slide()
+		return
 	process_animations()
 	var direction := Input.get_vector("left", "right", "up", "down")
 	if direction!=Vector2.ZERO:
@@ -17,15 +21,26 @@ func _physics_process(_delta: float) -> void:
 			velocity = direction * run_SPEED
 		else:
 			velocity = direction * walk_SPEED
+	
 	else:
 		velocity=Vector2.ZERO
+	if Input.is_action_just_pressed("attack"):
+			attack()
+	move_and_slide()
+		
+func attack() -> void:
+	if_attacking = true
+	velocity=Vector2.ZERO
+	play_animations("move_hit",check_direction)
+	await animated_sprite_2d.animation_finished
+	if_attacking = false
 
 
 
 
 
 	
-	move_and_slide()
+
 
 
 func process_animations() -> void:
@@ -34,6 +49,7 @@ func process_animations() -> void:
 			play_animations("run",check_direction)
 		else:
 			play_animations("move",check_direction)
+				
 	else:
 		play_animations("idle", check_direction)
 
