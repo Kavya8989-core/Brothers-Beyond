5203dda0c710389cdffe2debf54b5a53c36c5251
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-09 18:31:25 +0530

    added npc with animations

---
+++ npc_scene.gd
@@ -0,0 +1,75 @@
+extends CharacterBody2D
+
+
+const SPEED=70
+enum{
+	IDLE,
+	NEW_DIR,
+	MOVE
+}
+var current_state=IDLE
+var is_roaming = true
+var is_chatting = false
+
+var player
+var player_is_in_chat_zone=false
+
+var dir=Vector2.RIGHT
+var start_pos
+
+
+
+func _ready():
+	randomize()
+	start_pos = position
+func _process(delta: float) -> void:
+	if current_state == 0 or current_state ==1:
+		$AnimatedSprite2D.play("idle")
+		
+	elif current_state==2 and !is_chatting:
+		if dir.x>0 :
+			$AnimatedSprite2D.play("walking right")
+		if dir.x<0 :
+			$AnimatedSprite2D.play("walking left")
+		if dir.y<0:
+			$AnimatedSprite2D.play("walking up")
+		if dir.y>0:
+			$AnimatedSprite2D.play("walking down")
+			
+	
+	if is_roaming:
+		match current_state:
+			IDLE:
+				pass
+			NEW_DIR:
+				dir = choose([Vector2.RIGHT, Vector2.LEFT,Vector2.UP,Vector2.DOWN])
+			MOVE:
+				move(delta)
+				
+func choose(array):
+	array.shuffle()
+	return array.front()
+	
+func move(delta):
+	if !is_chatting:
+		velocity=dir*SPEED
+		move_and_slide()
+	else:
+		velocity=Vector2.ZERO
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player=body
+		player_is_in_chat_zone=true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.has_meta("player"):
+		player_is_in_chat_zone=false
+		
+
+
+
+func _on_timer_timeout() -> void:
+	$Timer.wait_time=choose([0.5,1,1.5])
+	current_state=choose([IDLE,NEW_DIR,MOVE])
