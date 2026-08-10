feea15da23686065a1f38b99b03cfc11d83cc302
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-10 18:47:13 +0530

    npc fixed

--- interaction/interaction area/interaction_scene.gd
+++ interaction/interaction area/interaction_scene.gd
@@ -2,18 +2,17 @@ extends Area2D
 
 class_name Interaction_area
 
-@export var action_name : String ="interact"
+@export var action_name: String = "interact"
 
-var interact : Callable  = func():
+var interact: Callable = func():
 	pass
 
 
-
-
-
 func _on_body_entered(body: Node2D) -> void:
-	InteractionManager.register_area(self)
+	if body.is_in_group("player"):
+		InteractionManager.register_area(self)
 
 
 func _on_body_exited(body: Node2D) -> void:
-	InteractionManager.unregister_area(self)
+	if body.is_in_group("player"):
+		InteractionManager.unregister_area(self)
