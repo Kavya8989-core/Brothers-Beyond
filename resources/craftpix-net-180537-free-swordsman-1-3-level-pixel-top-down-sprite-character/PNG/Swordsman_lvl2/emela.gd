d3179a87460a5da5939bf59ab320f8a26e74650a
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-14 07:36:19 +0530

    idle animation added

--- resources/craftpix-net-180537-free-swordsman-1-3-level-pixel-top-down-sprite-character/PNG/Swordsman_lvl2/emela.gd
+++ resources/craftpix-net-180537-free-swordsman-1-3-level-pixel-top-down-sprite-character/PNG/Swordsman_lvl2/emela.gd
@@ -7,7 +7,16 @@ var current_direction : String = "left"
 var is_in_range : bool =false
 
 func process_animaiton() -> void:
-	animated_sprite_2d.play("idle" + current_direction)
+	animated_sprite_2d.play("idle " + current_direction)
 
 func _process(delta: float) -> void:
 	process_animaiton()
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
