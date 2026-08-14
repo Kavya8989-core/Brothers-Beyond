d413d355385357e1ca15879a6906b58fa85f9196
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-14 08:00:18 +0530

    interaction added

--- resources/craftpix-net-180537-free-swordsman-1-3-level-pixel-top-down-sprite-character/PNG/Swordsman_lvl2/emela.gd
+++ resources/craftpix-net-180537-free-swordsman-1-3-level-pixel-top-down-sprite-character/PNG/Swordsman_lvl2/emela.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var label: Label = $Label
+@onready var player = get_tree().get_first_node_in_group("player")
 
 const SPEED = 300.0
 var current_direction : String = "left"
@@ -8,6 +9,18 @@ var is_in_range : bool =false
 
 func process_animaiton() -> void:
 	animated_sprite_2d.play("idle " + current_direction)
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and is_in_range:
+		if player.global_position.x > global_position.x:
+			current_direction = "right"
+		elif player.global_position.x < global_position.x:
+			current_direction = "left"
+		elif player.global_position.y < global_position.y:
+			current_direction = "up"
+		elif player.global_position.y > global_position.y:
+			current_direction = "down"
+		print("interacted")
 
 func _process(delta: float) -> void:
 	process_animaiton()
@@ -15,8 +28,12 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group("player"):
+		is_in_range=true
+		print("in range")
+		label.show()
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	label.hide()
+	pass
