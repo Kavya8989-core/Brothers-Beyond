14e3271a2af4f34502d4dc3f0479e332eba341c5
KAVYA SHARMA <ks2371008@gmail.com>
2026-08-16 22:47:06 +0530

    fox script added with the follow of mc after convo with npc

--- With_shadow/fox.gd
+++ With_shadow/fox.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
-
+@onready var player = get_tree().get_first_node_in_group("player")
 const walk_SPEED = 100.0
 const run_SPEED=200.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-func _process(delta: float) -> void:
-	print("fox reading ", DialogueManager.is_dialogue_over)
+func _physics_process(delta: float) -> void:
+	if DialogueManager.is_dialogue_over:
+		var direction = global_position.direction_to(player.global_position)
+		velocity = direction * walk_SPEED
+		animated_sprite_2d.play("walk left")
+		move_and_slide()
