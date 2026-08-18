1ce72afd46c9d80672a1126f2e577c142256f99e
Kavya8989-core <ks2371008@gmail.com>
2026-08-18 08:39:57 +0530

    fox bugs fix

--- With_shadow/fox.gd
+++ With_shadow/fox.gd
@@ -1,34 +1,44 @@
 extends CharacterBody2D
-@onready var player = get_tree().get_first_node_in_group("player")
-const walk_SPEED = 70.0
-const run_SPEED=200.0
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
+const walk_SPEED = 70.0
+const run_SPEED = 200.0
 
-	if DialogueManager.is_dialogue_over:
+var player: CharacterBody2D
 
-		var follow_distance = global_position.distance_to(player.global_position)
 
-		if follow_distance > 50:
-			var direction = global_position.direction_to(player.global_position)
+func _physics_process(_delta: float) -> void:
+	print("FOX: dialogue active = ", DialogueManager.is_dialogue_active)
 
-			velocity = direction * walk_SPEED
+	if player == null:
+		player = get_tree().get_first_node_in_group("player")
+		return
 
-			if abs(direction.x) > abs(direction.y):
-				if direction.x > 0:
-					animated_sprite_2d.play("walk right")
-				else:
-					animated_sprite_2d.play("walk left")
+	if DialogueManager.is_dialogue_active:
+		velocity = Vector2.ZERO
+		animated_sprite_2d.play("idle down")
+		return
+
+	var follow_distance = global_position.distance_to(player.global_position)
+
+	if follow_distance > 50:
+		var direction = global_position.direction_to(player.global_position)
+
+		velocity = direction * walk_SPEED
+
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				animated_sprite_2d.play("walk right")
 			else:
-				if direction.y > 0:
-					animated_sprite_2d.play("walk down")
-				else:
-					animated_sprite_2d.play("walk up")
-
-			move_and_slide()
+				animated_sprite_2d.play("walk left")
 		else:
-			velocity = Vector2.ZERO
+			if direction.y > 0:
+				animated_sprite_2d.play("walk down")
+			else:
+				animated_sprite_2d.play("walk up")
 
-			move_and_slide()
-			animated_sprite_2d.play("idle down")
+		move_and_slide()
+	else:
+		velocity = Vector2.ZERO
+		animated_sprite_2d.play("idle down")
