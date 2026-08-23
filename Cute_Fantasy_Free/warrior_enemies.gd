1d0d3e0efeec7670f541b3e6ac2c45bc9ddb19f7
Kavya8989-core <ks2371008@gmail.com>
2026-08-23 15:19:03 +0530

    enemy health system and death added

--- Cute_Fantasy_Free/warrior_enemies.gd
+++ Cute_Fantasy_Free/warrior_enemies.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var speed : int = 70
 var health : int = 50
@@ -8,6 +8,12 @@ var health : int = 50
 func takes_dmg() -> void:
 	if GameManager.mc_attacks == true:
 		health -= 20
-		print("function worked")
-		print(health)
 		GameManager.mc_attacks = false
+		if health <= 0 :
+			health = 0
+			animated_sprite.play("death")
+	print(health)
+
+func _ready() -> void:
+	animated_sprite.play("idle")
+	
