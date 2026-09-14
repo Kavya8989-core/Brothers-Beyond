d6b0fbaf4614ed7ce694634365034e6e387f17fd
Kavya8989-core <ks2371008@gmail.com>
2026-09-14 23:34:29 +0530

    scene 3 to 4 node change

--- scripts gd/scene_3.gd
+++ scripts gd/scene_3.gd
@@ -7,3 +7,12 @@ func _on_inventory_gui_closed() -> void:
 
 func _on_inventory_gui_opened() -> void:
 	get_tree().paused = true
+
+
+func _on_lev_4_promotion_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file('res://scenes/terrain.tscn')
+
+
+func _on_lev_4_promotion_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
